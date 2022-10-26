ba4c0f52218b71b7ba3ed0beab4486e7d1b6d6f4
Gorshunov2021 <gorshunov2.20@gmail.com>
2022-10-26 22:03:31 +0300

    26.10.2022

--- task0023_numbers_array_reverse/ArrayReverseMain.java
+++ task0023_numbers_array_reverse/ArrayReverseMain.java
@@ -1,20 +1,23 @@
 package task0023_numbers_array_reverse;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class ArrayReverseMain {
 	public static void main(String[] args) {
+		System.out.println("Enter count numbers Massive: ");
+		Scanner scan = new Scanner(System.in);
+		int L = scan.nextInt();
+		int[] arr = new int [L];
 
-		
-		int[] array1 = new int[] {};
-		System.out.println(Arrays.toString(array1) + " => ");
-		ArrayReverse.invert(array1);
-		System.out.println(Arrays.toString(array1));
+		System.out.println("Enter numbers: ");
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = scan.nextInt();
+		}
 
-		array1 = new int [] {0, 1, 2, 3, 4, 5};
-		System.out.println(Arrays.toString(array1) + " => ");
-		ArrayReverse.invert(array1);
-		System.out.println(Arrays.toString(array1));
+		System.out.println(Arrays.toString(arr));
+		ArrayReverse.invert(arr);
+		System.out.println(Arrays.toString(arr));
 
 	}
 }
